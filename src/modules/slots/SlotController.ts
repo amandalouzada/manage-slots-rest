1d0b140ff0ab138d51234dd60ffaebb8b5899e6a
Amanda Louzada <amandanuneslouzada@gmail.com>
2020-06-25 19:23:43 -0300

    add reserved into  service controller

--- src/modules/slots/SlotController.ts
+++ src/modules/slots/SlotController.ts
@@ -1,11 +1,12 @@
 import ISlotService from "./ISlotService";
 import { Request, Response } from 'express';
 import { ISlotsRequestDto } from "./ISlotsDTO";
+import IAvailabilityService from "./availability/IAvailabilityService";
 
 export class SlotController {
-  private slotService: ISlotService;
-  constructor(slotService: ISlotService) {
+  constructor(private slotService: ISlotService, private availabilityService: IAvailabilityService) {
     this.slotService = slotService;
+    this.availabilityService = availabilityService;
   }
 
   createMany = async (req: Request, res: Response): Promise<any> => {
@@ -21,7 +22,7 @@ export class SlotController {
   list = async (req: Request, res: Response): Promise<any> => {
     // const professionaId = res.locals.id;
     // const slotRequest = { ...req.body, professionaId } as ISlotsRequestDto;
-    
+
     const { start, end, professionalId } = req.query;
     const slots = await this.slotService.listSlotsByInterval(
       new Date(start.toString()),
@@ -29,7 +30,15 @@ export class SlotController {
     );
 
     res.json({ slots });
+  }
 
+  booksASeesion = async (req: Request, res: Response): Promise<any> => {
+    const { availabilityId, customerId } = req.body;
+    const availability = await this.availabilityService.booksASession(
+      availabilityId,
+      customerId
+    )
+    res.json({ availability })
   }
 
 }
--- src/modules/slots/index.ts
+++ src/modules/slots/index.ts
@@ -2,10 +2,12 @@ import { SlotRespository } from "./SlotRepository";
 import SlotModel from "@infra/mongoose/models/Slot";
 import { SlotService } from "./SlotService";
 import { SlotController } from "./SlotController";
+import AvailabilityService from "./availability/AvailabilityService";
 
 const slotRepository = new SlotRespository(SlotModel);
 const slotService = new SlotService(slotRepository);
-const slotController = new SlotController(slotService);
+const availabilityService = new AvailabilityService(slotRepository);
+const slotController = new SlotController(slotService, availabilityService);
 
 export {
   slotRepository,

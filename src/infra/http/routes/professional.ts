a16b6da73a306403060369b8a436ef0822028420
Amanda Louzada <amandanuneslouzada@gmail.com>
2020-06-25 19:40:15 -0300

    add middlewares

--- src/infra/http/routes/professional.ts
+++ src/infra/http/routes/professional.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { professionalController } from '@modules/professional';
+import { professionalMiddleware } from '@modules/auth/authMiddlewares';
 
 const ProfessionalRoutes = express.Router();
 
--- src/infra/http/routes/slot.ts
+++ src/infra/http/routes/slot.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { slotController } from '@modules/slots';
+import { professionalMiddleware, customerMiddleware } from '@modules/auth/authMiddlewares';
 
 const SlotRoutes = express.Router();
 
-SlotRoutes.post('/', slotController.createMany);
+SlotRoutes.post('/', professionalMiddleware, slotController.createMany);
 SlotRoutes.get('/', slotController.list);
+SlotRoutes.post('/booksASession', customerMiddleware, slotController.booksASeesion);
 
 export default SlotRoutes;
